ee4ef0cf24252eb5e5585d399ae6a711a8d73204
unknown <s.jumadil.akbar@gmail.com>
2023-04-05 22:57:10 +0700

    k

---
+++ storage/framework/views/0921320ebb84ce632763b3b98467c1ea83ab807d.php
@@ -0,0 +1,42 @@
+<button class="btn btn-secondary" type="button" data-bs-toggle="modal" data-original-title="test" data-bs-target="#exampleModal">Pesan</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Pesanan</h5>
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="mb-3" action="<?php echo e(route('pesan')); ?>" method="POST">
+                    <?php echo csrf_field(); ?>
+                    <div class="mx-3">
+                        <div class="col col-sm-12 ">
+                            <label class="col-sm-12 col-form-label">Atas Nama</label>
+                            <input class="form-control" type="text" name="nama" id="nama" placeholder="" required>
+                        </div>
+                        <div class="col col-sm-12 ">
+                            <label class="col-sm-12 col-form-label">Plat Mobil</label>
+                            <input class="form-control" type="text" name="plat" id="plat" placeholder="" required>
+                        </div>
+                        <div class="col col-sm-12 ">
+                            <label class="col-sm-12 col-form-label">Jenis Mobil</label>
+                            
+                            <select class="form-control" name="jenis" id="jenis">
+                                <option value="">-- Pilih Jenis Mobil --</option>
+                                <?php echo e($jenisMobil); ?>
+
+                            </select>
+                        </div>
+                        <div class="col col-sm-12 ">
+                            <label class="col-sm-12 col-form-label">No Handphone</label>
+                            <input class="form-control" type="text" name="hp" id="hp" placeholder="" >
+                        </div>
+                        <div class="modal-footer mt-3">
+                            <button class="btn btn-success" id="submit" type="submit">Pesan</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div><?php /**PATH C:\Aplikasi\Laravel\Aplikasi_Doorsmeer\resources\views/doorsmeer/components/pesan.blade.php ENDPATH**/ ?>
--- storage/framework/views/94db30ae7feca0fafd4b14eb5d6c7739ba017bd2.php
+++ storage/framework/views/94db30ae7feca0fafd4b14eb5d6c7739ba017bd2.php
@@ -1,5 +1,5 @@
-<button class="btn btn-secondary btn-sm" type="button" data-bs-toggle="modal" data-original-title="test" data-bs-target="#exampleModal{{$id}}">Diproses</button>
-<div class="modal fade" id="exampleModal{{$id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<button class="btn btn-secondary btn-sm" type="button" data-bs-toggle="modal" data-original-title="test" data-bs-target="#exampleModal<?php echo e($id); ?>">Diproses</button>
+<div class="modal fade" id="exampleModal<?php echo e($id); ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -8,12 +8,12 @@
             </div>
             <div class="modal-body">
                 <form class="mb-3" action="" method="POST">
-                    @csrf
-                    <input type="hidden" name="id" value="{{$id}}">
+                    <?php echo csrf_field(); ?>
+                    <input type="hidden" name="id" value="<?php echo e($id); ?>">
                     <div class="mx-3">
                         <div class="col col-sm-12 ">
                             <label class="col-sm-12 col-form-label">Total Pembayaran</label>
-                            <input class="form-control" type="number" name="bayar" id="bayar" placeholder="" value="{{ $harga ?? '' }}">
+                            <input class="form-control" type="number" name="bayar" id="bayar" placeholder="" value="<?php echo e($harga ?? ''); ?>">
                         </div>
                         <div class="modal-footer mt-3">
                             <button class="btn btn-success" id="submit" type="submit">Selesai</button>
@@ -23,4 +23,4 @@
             </div>
         </div>
     </div>
-</div>
+</div><?php /**PATH C:\Aplikasi\Laravel\Aplikasi_Doorsmeer\resources\views/doorsmeer/components/bayar.blade.php ENDPATH**/ ?>
